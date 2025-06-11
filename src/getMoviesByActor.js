c919df310d8b2d5a9998fec931bf591168c61336
Diogo Cardoso <digovegeta@gmail.com>
2025-06-11 20:41:20 -0300

    atividade 3

--- src/getMoviesByActor.js
+++ src/getMoviesByActor.js
@@ -1,8 +1,15 @@
 const movies = require("../data/movies");
 
 function getMoviesByActor(ator) {
- 
+  const out = movies.filter((ele) => {
+    return filme = ele.elenco.find(ele => {
+      return ele == ator 
+    })
+  })
+  return out;
 }
+
+console.log(getMoviesByActor("Robert Downey Jr."));
 
 module.exports = {
   getMoviesByActor

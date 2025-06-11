68aab1d2aec1fc0d8900e248154fb5db452ddb37
Diogo Cardoso <digovegeta@gmail.com>
2025-06-11 19:35:03 -0300

    atividade 2

--- src/getMovieById.js
+++ src/getMovieById.js
@@ -1,8 +1,12 @@
 const movies = require("../data/movies");
 
 function getMovieById(id) {
-  
+    const out = movies.find((ele) => {
+      return id === ele.id ? ele: 0;
+    })
+    return out
 }
+console.log(getMovieById('1'));
 
 module.exports = {
   getMovieById

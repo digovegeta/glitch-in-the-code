4e06a08ad0b4f3d705097a856e8a26a005060e73
Diogo Cardoso <digovegeta@gmail.com>
2025-06-12 15:40:24 -0300

    atividade 10

--- src/actorsInMovie.js
+++ src/actorsInMovie.js
@@ -1,9 +1,16 @@
 const movies = require("../data/movies");
 
 const actorsInMovie = (titulo) => {
-  
+  let out = movies.filter( ele => ele.titulo == titulo)
+  .map((ele) => {
+     
+    return ele.elenco[0] + ele.elenco[1]
+  })
+    out = out.length == 0 ? "Filme não encontrado." : out;
+  return out
 }
 
+console.log(actorsInMovie("Matrix"))
 module.exports = {
   actorsInMovie
 }
--- src/countGenres.js
+++ src/countGenres.js
@@ -1,9 +1,17 @@
 const movies = require("../data/movies");
 
-function countGenres(movies) {
- 
+function countGenres(movies) { 
+
+  const contagem = {};
+  const todosGeneros = movies.flatMap(ele => ele.genero)
+                             .sort((a,b) => a-b)
+                             .forEach(genero => {
+                                contagem[genero] = (contagem[genero] || 0) + 1;
+                              }); 
+  return contagem
 }
 
+console.log(countGenres(movies))
 module.exports = {
   countGenres
 }

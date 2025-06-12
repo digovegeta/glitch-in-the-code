58f0f7bb3237c9b478a110cdb6fa0c0da8a3b234
Diogo Cardoso <digovegeta@gmail.com>
2025-06-12 10:28:27 -0300

    atividade 7

--- src/availableMovies.js
+++ src/availableMovies.js
@@ -1,9 +1,16 @@
 const movies = require("../data/movies");
 
 const availableMovies = (movies) => {
- 
+ const out = movies.filter((ele) => {
+      return ele.disponivel
+    })
+    .map( (ele) => {    
+      return ele.titulo
+    })
+  return out
 }
 
+console.log(availableMovies(movies))
 module.exports = {
   availableMovies
 }

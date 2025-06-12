25ec66da87bd28a9f4bf76b159e16779f98dc181
Diogo Cardoso <digovegeta@gmail.com>
2025-06-12 10:20:21 -0300

    atividade 6

--- src/orderMoviesByYear.js
+++ src/orderMoviesByYear.js
@@ -1,8 +1,11 @@
 const movies = require("../data/movies");
 
 const orderMoviesByYear = (movies) => {
-  
+const out = movies.map(ele => ele).sort((a,b) => a.anoLancamento - b.anoLancamento)
+return out
 }
+
+console.log(orderMoviesByYear(movies));
 
 module.exports = {
   orderMoviesByYear

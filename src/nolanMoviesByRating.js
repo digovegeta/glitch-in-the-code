ba74663b74ea546f302b279a202f6ec8f9815ab2
Diogo Cardoso <digovegeta@gmail.com>
2025-06-12 10:37:24 -0300

    atividade 8

--- src/nolanMoviesByRating.js
+++ src/nolanMoviesByRating.js
@@ -1,9 +1,11 @@
 const movies = require("../data/movies");
 
 const nolanMoviesByRating = (movies) => {
- 
+ const out = movies.filter((ele) => 'Christopher Nolan' == ele.diretor)
+ .sort((a,b) => b.avaliacao - a.avaliacao)
+ return out
 }
-
+console.log(nolanMoviesByRating(movies))
 module.exports = {
   nolanMoviesByRating
 }

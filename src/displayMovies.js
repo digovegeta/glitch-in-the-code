c86a2d6bac55113b506caeb9961009e10c32127a
Diogo Cardoso <digovegeta@gmail.com>
2025-06-12 10:08:39 -0300

    atividade 5 - duvida no array movies, esta undefined

--- src/displayMovies.js
+++ src/displayMovies.js
@@ -1,8 +1,15 @@
 const { movies } = require("../data/list1");
 
 const displayMovies = (movies) => {
- 
+  const out = movies.map((ele) => {
+    return "Título: " + ele.titulo + " - Diretor: " + ele.diretor;
+  })
+ return out
 }
+
+/*console.log(displayMovies(movies));*/
+
+console.log(movies)
 
 module.exports = {
   displayMovies

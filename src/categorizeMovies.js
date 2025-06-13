70b52afd44aebb5811cfb917e38c583c924def1c
Diogo Cardoso <digovegeta@gmail.com>
2025-06-13 05:07:45 -0300

    atividade 13 - compreender formato + atividade retorna o solicitado, verificar array vazio

--- src/categorizeMovies.js
+++ src/categorizeMovies.js
@@ -1,9 +1,16 @@
 const movies = require("../data/movies");
 
 function categorizeMovies(movies, details = true) {
- 
+  if(movies.length == 0){
+      return {}
+  }
+  if(movies[0].genero.length == 0){
+    return {}
+  }
+ const out = movies.map(ele => ele.genero)//.sort((a,b) => a-b)
+ return out
 }
-
+console.log(categorizeMovies(movies));
 module.exports = {
   categorizeMovies
 }
--- src/listOverdueLoans.js
+++ src/listOverdueLoans.js
@@ -1,9 +1,30 @@
 const movies = require("../data/movies");
 
 function listOverdueLoans(movies) {
- 
+ const out = movies.filter(ele => ele.emprestimos.length != 0)
+                   .map(ele => {
+                     //console.log(new Date(ele.emprestimos[0].dataDevolucao) < new Date())
+                     if(new Date(ele.emprestimos[0].dataDevolucao) < new Date()){
+                        const atrasado = {
+                          dataDevolucao: "", 
+                          dataEmprestimo: "", 
+                          id: "",
+                          status: "",
+                          titulo: "", 
+                        }
+                        atrasado.id = ele.id
+                        atrasado.titulo = ele.titulo
+                        atrasado.dataEmprestimo = ele.emprestimos[0].dataEmprestimo
+                        atrasado.dataDevolucao = ele.emprestimos[0].dataDevolucao
+                        atrasado.status = "atrasado"
+                        return atrasado
+                    }else{
+                      return []
+                    }
+                  })
+ return out
 }
-
+console.log(listOverdueLoans(movies))
 module.exports = {
   listOverdueLoans
 }
